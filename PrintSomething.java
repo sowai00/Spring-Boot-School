5bd5b08aa4b2dca693d2abec847ff1eddeed2085
sowai00 <72919700+sowai00@users.noreply.github.com>
2022-11-04 10:55:47 +0800

    print

---
+++ PrintSomething.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrintSomething {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int cols;
+        String symbol = "";
+
+        System.out.println("row?");
+        rows = scanner.nextInt();
+        System.out.println("cols?");
+        cols = scanner.nextInt();
+        System.out.println("symbol?");
+        symbol = scanner.next();
+
+        for (int i = 0; i < rows; i++) {
+            System.out.println();
+            for (int j = 0; j < cols; j++) {
+                System.out.print(symbol);
+            }
+
+        }
+        scanner.close();
+    }
+}
