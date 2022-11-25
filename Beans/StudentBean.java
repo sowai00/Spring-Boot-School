f7e4a99a7ef931433ec89a9cb7ecff7003a44218
sowai00 <72919700+sowai00@users.noreply.github.com>
2022-11-25 16:11:05 +0800

    Bean

---
+++ Beans/StudentBean.java
@@ -0,0 +1,33 @@
+package Beans;
+
+public class StudentBean implements java.io.Serializable {
+    private String firstName = null;
+    private String lastName = null;
+    private int age = 0;
+
+    public StudentBean() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
